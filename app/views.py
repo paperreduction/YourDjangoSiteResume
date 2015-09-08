52caad0c16d2a3ff11a70883e8a0e82553737b8c
Dan Dietz <danddietz@gmail.com>
2015-09-08 11:03:51 -0500

    update

--- app/views.py
+++ app/views.py
@@ -8,12 +8,12 @@ from django.template import RequestContext
 from datetime import datetime
 
 YOUR_INFO = {
-    'name' : 'Your name',
-    'bio' : 'What\'s your deal? What do you do?',
+    'name' : 'Dietz',
+    'bio' : 'Contributor to FabricBolt web based Fabric Deployment system. ',
     'email' : '', # Leave blank if you'd prefer not to share your email with other conference attendees
-    'twitter_username' : 'tweettweet', # No @ symbol, just the handle.
-    'github_username' : "fetchpush", 
-    'headshot_url' : '', # Link to your GitHub, Twitter, or Gravatar profile image.
+    'twitter_username' : '', # No @ symbol, just the handle.
+    'github_username' : "paperreduction", 
+    'headshot_url' : 'https://avatars2.githubusercontent.com/u/415984?v=3&s=460', # Link to your GitHub, Twitter, or Gravatar profile image.
 }
     
 def home(request):
